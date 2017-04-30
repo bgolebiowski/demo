0f36dc4d882aec7cba75b1ef2975886e0978ebd4
Bartosz Gołębiowski <Bartosz Gobiowski>
2017-04-30 15:18:46 +0200

    Initial commit

---
+++ src/main/java/com/example/BrokemProjectApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrokemProjectApplication.class, args);
+	}
+}
